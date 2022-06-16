b1608792746d309311299ff826a22547cc0fa1e3
maruakinu <marloaquino080621@gmail.com>
2022-06-16 22:17:21 +0800

    Final Commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 const port = 5555;
 
-//Creation of server with http method.
+//Creation of server 
 const server = http.createServer(app);
 
 //This server will listen to the port 555
